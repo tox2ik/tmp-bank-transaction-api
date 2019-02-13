da5d205fbf1d33c0850cf198a5abde6b86e710cf
Jaroslav Rakhmatoullin <jazzoslav@gmail.com>
2019-02-13 20:21:47 +0100

    new(model) generate entities

---
+++ src/Entity/BankTransaction.php
@@ -0,0 +1,46 @@
+<?php
+
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * BankTransaction
+ *
+ * @ORM\Table(name="bank_transaction", indexes={@ORM\Index(name="bt_uuid", columns={"uuid"})})
+ * @ORM\Entity
+ */
+class BankTransaction
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="bigint", nullable=false, options={"unsigned"=true})
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="uuid", type="string", length=36, nullable=true)
+     */
+    private $uuid;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="amount", type="decimal", precision=22, scale=2, nullable=true)
+     */
+    private $amount;
+
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="booking_date", type="datetime", nullable=true)
+     */
+    private $bookingDate;
+
+
+}
